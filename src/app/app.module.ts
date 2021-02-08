5c0545fa651b54fb51981809271502fac56ea1dd
trimijosealejandro93 <trimijosealejandro@yahoo.com>
2021-02-08 09:19:31 -0430

    nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './hero/hero.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    HeroDetailComponent,
-    MessagesComponent,
   ],
   imports: [
     BrowserModule,
